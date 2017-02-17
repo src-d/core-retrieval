9f1398388954cb6997cce813a12408e2b435d040
Santiago M. Mola <santi@mola.io>
2017-02-17 17:20:01 +0100

    add repository RootedTransactioner

---
+++ repository/repository.go
@@ -0,0 +1,167 @@
+// Package repository implements services to work with Git repository storage.
+package repository
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"srcd.works/go-billy.v1"
+	"srcd.works/go-git.v4"
+	"srcd.works/go-git.v4/plumbing"
+	"srcd.works/go-git.v4/storage/filesystem"
+)
+
+// RootedTransactioner can initiate transactions on rooted repositories.
+type RootedTransactioner interface {
+	Begin(h plumbing.Hash) (Tx, error)
+}
+
+// Tx is a transaction on a repository. Any change performed in the given
+// repository storer is in a transaction context. Transactions are guaranteed
+// to be isolated.
+type Tx interface {
+	// Storer gets the repository storer. It returns the same instance on
+	// every call until Commit or Rollback is performed.
+	Storer() git.Storer
+	// Commit commits all changes to the repository.
+	Commit() error
+	// Rollback undoes any changes and cleans up.
+	Rollback() error
+}
+
+type fsSrv struct {
+	fs    billy.Filesystem
+	local billy.Filesystem
+}
+
+// NewFilesystemRootedTransactioner returns a RootedTransactioner for repositories
+// stored in the given billy.Filesystem, and uses a second billy.Filesystem
+// as temporary storage for in-progress transactions.
+//
+// Note that transactionality is not fully guaranteed by this implementation,
+// since it relies on recursive copying between arbitrary filesystems. If a
+// Commit operation fails, the state of the first filesystem is unknown and can
+// be invalid.
+func NewFilesystemRootedTransactioner(fs, local billy.Filesystem) RootedTransactioner {
+	return &fsSrv{fs, local}
+}
+
+func (s *fsSrv) Begin(h plumbing.Hash) (Tx, error) {
+	origPath := h.String()
+	tmpPath := fmt.Sprintf("%s/%d", h.String(), time.Now().UnixNano())
+	if err := copyRecursive(s.fs, s.local, origPath, tmpPath); err != nil {
+		return nil, err
+	}
+
+	sto, err := filesystem.NewStorage(s.local.Dir(tmpPath))
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := git.Open(sto, nil); err == git.ErrRepositoryNotExists {
+		if _, err := git.Init(sto, nil); err != nil {
+			return nil, err
+		}
+	}
+
+	return &fsTx{
+		fs:       s.fs,
+		local:    s.local,
+		origPath: origPath,
+		tmpPath:  tmpPath,
+		s:        sto,
+	}, nil
+}
+
+type fsTx struct {
+	fs, local         billy.Filesystem
+	tmpPath, origPath string
+	s                 git.Storer
+}
+
+func (tx *fsTx) Storer() git.Storer {
+	return tx.s
+}
+
+func (tx *fsTx) Commit() error {
+	if err := copyRecursive(tx.local, tx.fs, tx.tmpPath, tx.origPath); err != nil {
+		_ = tx.cleanUp()
+		return err
+	}
+
+	return tx.cleanUp()
+}
+
+func (tx *fsTx) Rollback() error {
+	return tx.cleanUp()
+}
+
+func (tx *fsTx) cleanUp() error {
+	return billy.RemoveAll(tx.local, tx.tmpPath)
+}
+
+func copyRecursive(fromFs, toFs billy.Filesystem, from, to string) (err error) {
+	srcInfo, err := fromFs.Stat(from)
+	if os.IsNotExist(err) {
+		return nil
+	}
+
+	if err != nil {
+		return err
+	}
+
+	if srcInfo.Mode().IsRegular() {
+		return copyFile(fromFs, toFs, from, to)
+	}
+
+	if !srcInfo.IsDir() {
+		return fmt.Errorf("not a regular file or dir: %s", from)
+	}
+
+	fis, err := fromFs.ReadDir(from)
+	if err != nil {
+		return err
+	}
+
+	for _, fi := range fis {
+		fromPath := fromFs.Join(from, fi.Name())
+		toPath := toFs.Join(to, fi.Name())
+		if fi.IsDir() {
+			err := copyRecursive(fromFs, toFs, fromPath, toPath)
+			if err != nil {
+				return err
+			}
+		}
+
+		if err := copyFile(fromFs, toFs, fromPath, toPath); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func copyFile(fromFs, toFs billy.Filesystem, from, to string) (err error) {
+	src, err := fromFs.Open(from)
+	if err != nil {
+		return err
+	}
+	defer checkClose(src, &err)
+
+	dst, err := toFs.OpenFile(to, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, os.FileMode(0644))
+	if err != nil {
+		return err
+	}
+	defer checkClose(dst, &err)
+
+	_, err = io.Copy(dst, src)
+	return err
+}
+
+func checkClose(c io.Closer, err *error) {
+	if cerr := c.Close(); cerr != nil && *err == nil {
+		*err = cerr
+	}
+}
---
+++ repository/repository_test.go
@@ -0,0 +1,196 @@
+package repository
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"srcd.works/go-billy.v1"
+	"srcd.works/go-billy.v1/memfs"
+	"srcd.works/go-billy.v1/osfs"
+	"srcd.works/go-git.v4"
+	"srcd.works/go-git.v4/plumbing"
+)
+
+const (
+	tmpPrefix = "core-retrieval-test"
+)
+
+var (
+	h1 = plumbing.NewHash("0000000000000000000000000000000000000001")
+)
+
+func TestFilesystemSuite(t *testing.T) {
+	suite.Run(t, &FilesystemSuite{})
+}
+
+type FilesystemSuite struct {
+	suite.Suite
+	tmpDirs map[string]bool
+}
+
+func (s *FilesystemSuite) SetupTest() {
+	s.tmpDirs = make(map[string]bool)
+}
+
+func (s *FilesystemSuite) TearDownTest() {
+	s.cleanUpTempDirectories()
+}
+
+func (s *FilesystemSuite) cleanUpTempDirectories() {
+	require := require.New(s.T())
+	var err error
+	for dir := range s.tmpDirs {
+		if e := os.RemoveAll(dir); e != nil && err != nil {
+			err = e
+		}
+
+		delete(s.tmpDirs, dir)
+	}
+
+	require.NoError(err)
+}
+
+func (s *FilesystemSuite) Test() {
+	fsPairs := []*fsPair{
+		{"mem to mem", memfs.New(), memfs.New()},
+		{"mem to os", memfs.New(), s.newFilesystem()},
+		{"os to mem", s.newFilesystem(), memfs.New()},
+		{"os to os", s.newFilesystem(), s.newFilesystem()},
+	}
+
+	for _, fsPair := range fsPairs {
+		s.T().Run(fsPair.Name, func(t *testing.T) {
+			testRootedTransactioner(t, NewFilesystemRootedTransactioner(fsPair.From, fsPair.To))
+		})
+	}
+}
+
+type invalidStatFs struct {
+	billy.Filesystem
+	statCalls int
+}
+
+func (fs *invalidStatFs) Stat(path string) (billy.FileInfo, error) {
+	fs.statCalls += 1
+	return nil, fmt.Errorf("mock error")
+}
+
+func (s *FilesystemSuite) TestInvalidStatFilesystem() {
+	fs := &invalidStatFs{Filesystem: s.newFilesystem()}
+	srv := NewFilesystemRootedTransactioner(fs, memfs.New())
+	tx, err := srv.Begin(h1)
+	s.EqualError(err, "mock error")
+	s.Equal(1, fs.statCalls)
+	s.Nil(tx)
+}
+
+type nonRegularFs struct {
+	billy.Filesystem
+	statCalls int
+}
+
+type nonRegularFileInfo struct {
+	Path string
+}
+
+func (f *nonRegularFileInfo) Name() string       { return f.Path }
+func (f *nonRegularFileInfo) Size() int64        { return 0 }
+func (f *nonRegularFileInfo) Mode() os.FileMode  { return os.ModeNamedPipe }
+func (f *nonRegularFileInfo) ModTime() time.Time { return time.Now() }
+func (f *nonRegularFileInfo) IsDir() bool        { return false }
+func (f *nonRegularFileInfo) Sys() interface{}   { return nil }
+
+func (fs *nonRegularFs) Stat(path string) (billy.FileInfo, error) {
+	fs.statCalls += 1
+	return &nonRegularFileInfo{path}, nil
+}
+
+func (s *FilesystemSuite) TestNonRegularFileFilesystem() {
+	fs := &nonRegularFs{Filesystem: s.newFilesystem()}
+	srv := NewFilesystemRootedTransactioner(fs, memfs.New())
+	tx, err := srv.Begin(h1)
+	s.Error(err)
+	s.True(strings.HasPrefix(err.Error(), "not a regular file or dir"))
+	s.Equal(1, fs.statCalls)
+	s.Nil(tx)
+}
+
+func (s *FilesystemSuite) newFilesystem() billy.Filesystem {
+	require := require.New(s.T())
+	tmpDir, err := ioutil.TempDir(os.TempDir(), tmpPrefix)
+	require.NoError(err)
+	s.tmpDirs[tmpDir] = true
+	return osfs.New(tmpDir)
+}
+
+func testRootedTransactioner(t *testing.T, s RootedTransactioner) {
+	require := require.New(t)
+
+	// begin tx1
+	tx1, err := s.Begin(h1)
+	require.NoError(err)
+	require.NotNil(tx1)
+	r1, err := git.Open(tx1.Storer(), nil)
+	require.NoError(err)
+
+	// tx1 -> create ref1
+	refName1 := plumbing.ReferenceName("ref1")
+	err = r1.Storer.SetReference(plumbing.NewSymbolicReference(refName1, refName1))
+	require.NoError(err)
+
+	// begin tx2
+	tx2, err := s.Begin(h1)
+	require.NoError(err)
+	require.NotNil(tx2)
+	r2, err := git.Open(tx2.Storer(), nil)
+	require.NoError(err)
+
+	// ref1 not visible in tx2
+	_, err = r2.Reference(refName1, false)
+	require.Equal(plumbing.ErrReferenceNotFound, err)
+
+	// tx2 -> create ref2
+	refName2 := plumbing.ReferenceName("ref2")
+	err = r2.Storer.SetReference(plumbing.NewSymbolicReference(refName2, refName2))
+	require.NoError(err)
+
+	// ref2 not visible in tx2
+	_, err = r1.Reference(refName2, false)
+	require.Equal(plumbing.ErrReferenceNotFound, err)
+
+	// commit tx1
+	err = tx1.Commit()
+	require.NoError(err)
+
+	// ref1 not visible in tx2 (even with tx1 committed)
+	_, err = r2.Reference(refName1, false)
+	require.Equal(plumbing.ErrReferenceNotFound, err)
+
+	// rollback tx2
+	err = tx2.Rollback()
+	require.NoError(err)
+
+	// begin tx3
+	tx3, err := s.Begin(h1)
+	require.NoError(err)
+	require.NotNil(tx3)
+	r3, err := git.Open(tx1.Storer(), nil)
+	require.NoError(err)
+
+	// ref1 visible in tx3
+	_, err = r3.Reference(refName1, false)
+	require.NoError(err)
+	require.NoError(tx3.Rollback())
+}
+
+type fsPair struct {
+	Name string
+	From billy.Filesystem
+	To   billy.Filesystem
+}
